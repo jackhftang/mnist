2bf99a71ae8e276dd800a777d5a12fd99390ee3c
Jack Tang <jackhftang@hotmail.com>
2016-04-23 15:46:46 +0800

    get_started

---
+++ tensorflow_get_started.py
@@ -0,0 +1,58 @@
+
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+import tensorflow as tf
+
+###########################################################
+
+## declare placeholder and variables
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.initialize_all_variables())
+
+
+## declare network structure
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+
+## training
+print('training...');
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+for i in range(1000):
+  batch = mnist.train.next_batch(50)
+  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+###########################################################
+
+print('predicting...');
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+# result = sess.run(tf.argmax(y,1), feed_dict={x: mnist.validation.images})
+# print(' '.join(map(str, result)) )
+
